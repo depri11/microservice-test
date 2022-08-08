8446cf298653d95ce790939749bc1d9d2e1093c8
devriwirlandika <80185253+depri11@users.noreply.github.com>
2022-08-08 23:28:22 +0700

    setup connect database

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/depri11/lolipad/src/configs"
+)
+
+func main() {
+	_, err := configs.SetupDB()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Database connect")
+}
--- src/configs/db.go
+++ src/configs/db.go
@@ -1,1 +1,37 @@
 package configs
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func SetupDB() (*sql.DB, error) {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	user := os.Getenv("DB_USER")
+	host := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+	password := os.Getenv("DB_PASS")
+
+	con := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", host, user, password, dbName)
+
+	db, err := sql.Open("postgres", con)
+	if err != nil {
+		panic(err)
+	}
+
+	db.SetConnMaxIdleTime(10)
+	db.SetMaxOpenConns(100)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+	db.SetConnMaxLifetime(time.Hour)
+
+	return db, nil
+}
