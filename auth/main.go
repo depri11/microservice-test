3bab5966616f9bfee39924eb20ef882d5fda8297
devriwirlandika <80185253+depri11@users.noreply.github.com>
2022-08-09 08:09:13 +0700

    setup main server

--- auth/main.go
+++ auth/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/depri11/lolipad/auth/src/routes"
+)
+
+func main() {
+	if mainRoute, err := routes.SetupRouter(); err == nil {
+		var addrs string = "0.0.0.0:3000"
+
+		if pr := os.Getenv("PORT"); pr != "" {
+			addrs = "0.0.0.0:" + pr
+		}
+
+		if err := http.ListenAndServe(addrs, mainRoute); err != nil {
+			panic(err)
+		}
+	}
+
+}
